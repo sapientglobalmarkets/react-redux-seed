5fca377fdd4f24b1d8f4c380645c92033adb6d62
nareshbhatia <naresh.archfirst@gmail.com>
2016-06-26 19:29:23 -0400

    Implement stateful component generator

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,4 +1,4 @@
 module.exports = function (plop) {
     plop.setGenerator('component', require('./component'));
-    plop.setGenerator('feature', require('./feature'));
+    // plop.setGenerator('feature', require('./feature'));
 };
