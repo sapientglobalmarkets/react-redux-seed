54f60117bb3729d1f5b02c70138f60e147238ccb
nareshbhatia <naresh.archfirst@gmail.com>
2016-07-02 00:22:10 -0400

    Fixed tests

--- src/clock/components/clock-page.test.jsx
+++ src/clock/components/clock-page.test.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
 import {ClockPage} from './clock-page';
 
-describe('<Clock>', ()=> {
-    beforeEach(()=> {
+describe('<Clock>', () => {
+
+    beforeEach(() => {
         this.store = { clock: {date: new Date()} };
     });
 
-    it('should render correctly', ()=> {
-        const dom = shallow(<ClockPage store={this.store}/>);
+    it('should render correctly', () => {
+        const dom = shallow(
+            <Provider store={this.store}>
+                <ClockPage/>
+            </Provider>
+        );
         expect(dom).to.have.length(1);
     });
 
-    it('should render the time in <h2>', ()=> {
-        const dom = mount(<ClockPage store={this.store}/>);
-        expect(dom.find('h2')).to.have.length(1);
+    it('should render the time in <h2>', () => {
+        const dom = mount(
+            <Provider store={this.store}>
+                <ClockPage/>
+            </Provider>
+        );
+        expect(dom.find('h2').length).to.be.greaterThan(0);
     });
+
 });
